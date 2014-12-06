abfe64e24d9683bd038ea20eaccdb32008d94875
vuylov <vuylov@gmail.com>
2014-12-06 11:59:31 +0300

    Added access control for admin controller

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -8,6 +8,7 @@ use app\models\RealtySearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * AdminController implements the CRUD actions for Realty model.
@@ -17,6 +18,15 @@ class AdminController extends Controller
     public function behaviors()
     {
         return [
+            'access'    => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@']
+                    ]
+                ]
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
