b1a9a36326d39d52d341f43c970fa76e56d1aad4
vuylov <vuylov@gmail.com>
2014-12-30 10:45:04 +0300

    Add status in widget and new handle

--- components/NewOffer.php
+++ components/NewOffer.php
@@ -7,10 +7,12 @@
  */
 
 namespace app\components;
+use app\models\Status;
 use Yii;
 use yii\base\Widget;
 use yii\helpers\Url;
 use app\models\Realty;
+use yii\helpers\VarDumper;
 use yii\web\HttpException;
 
 class NewOffer extends Widget{
@@ -31,28 +33,36 @@ class NewOffer extends Widget{
     {
 
         if($this->mode === NewOffer::MODE_LAST){
-            $realties = Realty::find()->with(['files', 'region'])->orderBy(['create_time' => SORT_DESC])->limit($this->count)->all();
+            $realties = Realty::find()->with(['files', 'region', 'region', 'type'])->where(['status' => Status::ACTIVE])->orderBy(['create_time' => SORT_DESC])->limit($this->count)->all();
         }
         elseif($this->mode === NewOffer::MODE_SIMILAR){
             if($this->model === null)
                 throw new HttpException(500, 'Bad parameters');
 
-            $realties = Realty::find()->with(['files', 'region'])->where([
-                'type_id'   => $this->model->type_id,
-                'region_id' => $this->model->region_id
+            $realties = Realty::find()->with(['files', 'region', 'region', 'type'])->where(
+                'status = :status AND type_id = :type AND region_id = :region AND id <> :id', [
+                ':status'   => Status::ACTIVE,
+                ':type'     => $this->model->type_id,
+                ':region'   => $this->model->region_id,
+                ':id'       => $this->model->id
             ])->orderBy(['create_time' => SORT_DESC])->limit($this->count)->all();
         }
 
-        $html = '<ul class="offers">';
-        foreach($realties as $realty){
-            $html.='<li><div class="img-container"><a href="'.Url::to(['realty/view', 'type' => $realty->type_id, 'id' => $realty->id]).'"><img src="'.$realty->randomImg.'" width="218px" class="img-responsive img-thumbnail"></a>';
-            $html.='<div class="realty-price">'.$realty->price.' руб.</div></div>';
-            $html.='<div class="small">'.$realty->type->name.'</div>';
-            $html.='<div class="small">'.$realty->region->name.' р-н</div>';
-            $html.='<div class="small">'.$realty->address.'</div>';
-            $html.='</li>';
+        if(count($realties) > 0){
+            $html = '<ul class="offers">';
+            foreach($realties as $realty){
+                $html.='<li><div class="img-container"><a href="'.Url::to(['realty/view', 'type' => $realty->type_id, 'id' => $realty->id]).'"><img src="'.$realty->randomImg.'" width="218px" class="img-responsive img-thumbnail"></a>';
+                $html.='<div class="realty-price">'.$realty->price.' руб.</div></div>';
+                $html.='<div class="small">'.$realty->type->name.'</div>';
+                $html.='<div class="small">'.$realty->region->name.' р-н</div>';
+                $html.='<div class="small">'.$realty->address.'</div>';
+                $html.='</li>';
+            }
+            $html.='</ul>';
         }
-        $html.='</ul>';
+        else{
+            $html = '<div>Предложений нет</div>';
+        }
         return $html;
     }
 
