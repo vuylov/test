cc77b7a9ff3bf7494b7352f531adda2b8efb30af
Sumarokov <srg@vm91842.local>
2015-03-25 13:57:58 +0300

    Server test commit

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=agency',
+    'username' => 'agency',
+    'password' => 'Agency_2015',
     'charset' => 'utf8',
 ];
