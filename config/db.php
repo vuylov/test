5e21ac2120d84b42d1f2229b84ebdf3be862caf7
vuylov <vuylov@gmail.com>
2015-04-13 14:31:06 +0300

    Fix login view

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=agency',
-    'username' => 'agency',
-    'password' => 'Agency_2015',
-    'charset' => 'utf8',
-];
